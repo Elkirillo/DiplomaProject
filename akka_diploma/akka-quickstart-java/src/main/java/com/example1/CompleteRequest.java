bb3b62567ee09b76b31361c3fa6b8099a639d436
Elkirillo <kirill.gora@mail.ru>
2022-06-21 22:23:43 +0300

    Diploma part1

---
+++ akka_diploma/akka-quickstart-java/src/main/java/com/example1/CompleteRequest.java
@@ -0,0 +1,33 @@
+package com.example1;
+
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.Behavior;
+import akka.actor.typed.javadsl.AbstractBehavior;
+import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.actor.typed.javadsl.Receive;
+
+public class CompleteRequest extends AbstractBehavior<CompleteRequest.Requests> {
+
+    public static class Requests {
+        public Requests(){}
+    }
+    private CompleteRequest(ActorContext<CompleteRequest.Requests> context) {
+        super(context);
+    }
+
+    public static Behavior<Requests> create() { return Behaviors.setup(CompleteRequest::new); }
+
+    @Override
+    public Receive<Requests> createReceive() {
+        return newReceiveBuilder().onMessage(Requests.class, this::setResolved).build();
+    }
+
+    private Behavior<Requests> setResolved(Requests command) {
+        getContext().getLog().info("Отчет по заявке составлен");
+        ActorRef<BehaviorForRequest.Status> behaviorActor = getContext().spawn(BehaviorForRequest.create(), "actor5");
+        behaviorActor.tell(BehaviorForRequest.Status.RESOLVED);
+        return this;
+    }
+
+}
